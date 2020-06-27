fea0536b22e03081d5cce2db5ac3aac4afd1cd7e
Rjab_Chaalia <rjab.chaalia@isimg.tn>
2020-06-27 17:24:57 +0100

    add tests

--- shop/tests.py
+++ shop/tests.py
@@ -3,25 +3,27 @@ from selenium import webdriver
 from cart.forms import CartAddProductForm
 import hashlib
 from .models import Product, Category
+from django.core.exceptions import ValidationError
 
-# class FunctionalTestCase(TestCase):
-#
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
 
-# def test_is_there_home_page(self):
-#     self.browser.get("http://127.0.0.1:8000/en")
-#     # text = self.browser.find_element_by_id("text")
-#     # print(text)
-#     # text.send_keys("hello")
-#     # self.browser.find_element_by_name('submit').click()
-#     # self.assertIn('install', self.browser.page_source)
-#     c = self.browser.page_source.find('install')
-#     print(c)
-#     assert c
-#
-# def tearDown(self):
-#     self.browser.quit()
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_is_there_home_page(self):
+        self.browser.get("http://127.0.0.1:8000/en")
+        # text = self.browser.find_element_by_id("id_text")
+        # print(text)
+        # text.send_keys("hello")
+        # self.browser.find_element_by_name('submit').click()
+        # self.assertIn('install', self.browser.page_source)
+        c = self.browser.page_source.find('install')
+        # print(c)
+        assert c
+
+    def tearDown(self):
+        self.browser.quit()
 
 
 class unitTestCase(TestCase):
@@ -33,13 +35,12 @@ class unitTestCase(TestCase):
         self.assertTemplateUsed(response, 'shop/products/list.html')
 
     def test_cart_form(self):
-        form = CartAddProductForm(data={'quantity':15})
+        form = CartAddProductForm(data={'quantity': 15})
         self.assertTrue(form.is_valid())
 
     def test_hash_text(self):
         hashtext = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
         self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', hashtext)
-
 
     def test_prod(self):
         product = Product()
@@ -63,3 +64,9 @@ class unitTestCase(TestCase):
         response = self.client.get("/en/cart/")
         self.assertEqual(response.status_code, 200)
 
+    def test_bad_data(self):
+        def badProd():
+            prod = Product()
+            prod.name = 23
+            prod.full_clean()
+            self.assertRaises(ValidationError, badProd)
