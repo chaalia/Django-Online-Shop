d4291e349a760e04197503bdec765faf70eb33ab
Rjab_Chaalia <rjab.chaalia@isimg.tn>
2020-06-27 14:27:33 +0100

    add tests

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,18 +1,3 @@
-"""myshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,6 +7,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 urlpatterns = i18n_patterns(
+    path(_('jet/dashboard/'), include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
+    path((_('jet/')), include('jet.urls', 'jet')),  # Django JET URLS
     path("admin/", admin.site.urls),
     path(_("orders/"), include("orders.urls", namespace="orders")),
     path(_("paypal/"), include("paypal.standard.ipn.urls")),
@@ -35,5 +22,6 @@ urlpatterns = i18n_patterns(
 
 
 if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [path('__debug__/', include(debug_toolbar.urls))]
+    # import debug_toolbar
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+                   # + [path('__debug__/', include(debug_toolbar.urls))]
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from selenium import webdriver
+from cart.forms import CartAddProductForm
+import hashlib
+from .models import Product, Category
 
-# Create your tests here.
+# class FunctionalTestCase(TestCase):
+#
+#     def setUp(self):
+#         self.browser = webdriver.Firefox()
+
+# def test_is_there_home_page(self):
+#     self.browser.get("http://127.0.0.1:8000/en")
+#     # text = self.browser.find_element_by_id("text")
+#     # print(text)
+#     # text.send_keys("hello")
+#     # self.browser.find_element_by_name('submit').click()
+#     # self.assertIn('install', self.browser.page_source)
+#     c = self.browser.page_source.find('install')
+#     print(c)
+#     assert c
+#
+# def tearDown(self):
+#     self.browser.quit()
+
+
+class unitTestCase(TestCase):
+
+    def test_home_homepage(self):
+        response = self.client.get('/en/')
+        print(response.status_code)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shop/products/list.html')
+
+    def test_cart_form(self):
+        form = CartAddProductForm(data={'quantity':15})
+        self.assertTrue(form.is_valid())
+
+    def test_hash_text(self):
+        hashtext = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', hashtext)
+
+
+    def test_prod(self):
+        product = Product()
+        category = Category()
+        category.name = "tsts"
+        category.save()
+        product.category = category
+        product.name = "test"
+        product.slug = "test"
+        product.price = "150"
+        product.save()
+        return product
+
+    def test_product_object(self):
+        product = self.test_prod()
+        test_prod = Product.objects.get(name="test")
+        self.assertEqual(product.name, str(test_prod))
+
+    def test_viewing_cart(self):
+        # product = self.test_prod()
+        response = self.client.get("/en/cart/")
+        self.assertEqual(response.status_code, 200)
+
